92367a7005b8cc88c93a99210fc89f7bfb49dd70
Neha Merin <137706982+neha-merin@users.noreply.github.com>
2026-09-13 09:25:59 +0530

    Time: 11 ms (77.52%), Space: 19.5 MB (46.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -8,9 +8,8 @@ class Solution:
             countS[ch]=1+countS.get(ch,0)
         for ch in t:
             countT[ch]=1+countT.get(ch,0)
-        for ch in countS:
-            if countT!=countS:
-                return False
+        if countT!=countS:
+            return False
         return True
         
         
